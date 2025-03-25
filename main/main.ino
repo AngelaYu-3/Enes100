5be2c927333e4c13bda038af986509c92006e0f2
AngelaYu-3 <yuangela12@gmail.com>
2025-03-25 11:00:38 -0400

    modified main.ino

---
+++ main/main.ino
@@ -0,0 +1,13 @@
+#include "Enes100.h"
+#include "Print.h"
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  print_test();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Serial.println("hello world");
+}
--- src/print_test.ino
+++
@@ -1,4 +0,0 @@
-void print() {
-  print("hello world!");
-}
-
