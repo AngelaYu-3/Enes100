3d41ad844a29daa948692ad947df68d238948d6a
annatzeng <166579254+annatzeng@users.noreply.github.com>
2025-04-01 10:23:14 -0400

    fixedSetAngle

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,14 @@
+#include <Tank.h>
 #include "Enes100.h"
 #include "Print.h"
+
+// run once, aka main function
 void setup() {
   // ANNA TEST COMMIT
-  
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  print_test();
 
+  // put your setup code here, to run once:
+  Enes100.begin();
+  Enes100.print("Space Crash!");
 }
 
 void loop() {
